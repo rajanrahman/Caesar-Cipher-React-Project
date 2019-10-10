e82a0531401139f1fd4c6c21d27801699f446cb0
rajanrahman <36062541+rajanrahman@users.noreply.github.com>
2019-10-10 12:27:53 -0700

    Caesar Cipher now works for any shift constant

--- src/App.js
+++ src/App.js
@@ -1,102 +1,71 @@
 import React, { Component } from "react";
-import "./App.css";
+import Persons from "../Components/Persons/Persons";
+import Cockpit from "../Components/Cockpit/Cockpit";
 
-import Validation from "./ValidationComponent";
-import Char from "./CharComponent";
+import classes from "./App.css";
 
 class App extends Component {
   state = {
-    userInput: "",
-    shift: "1",
-    encodedOutput: ""
+    persons: [
+      { id: "8676", name: "Max", age: 27 },
+      { id: "8654", name: "Maxine", age: 227 },
+      { id: "7844", name: "Sdx", age: 137 }
+    ],
+    showPersons: false
   };
-  outputHandler = event => {
-    const userInput = event.target.value;
+  //handler for evens
+  deletePersonHandler = index => {
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
     this.setState({
-      userInput: userInput
-    });
-  };
-  deleteCharHandler = index => {
-    const text = this.state.userInput.split("");
-    text.splice(index, 1);
-    const ui = text.join("");
-    this.setState({ userInput: ui });
-  };
-  shiftChangeHandler = event => {
-    const newShift = event.target.value;
-    this.setState({
-      shift: newShift
+      persons: persons
     });
   };
 
-  encode = () => {
-    const shift = parseInt(this.state.shift, 10);
-    const text = this.state.userInput;
-    let output = "";
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
 
-    for (let i = 0; i < text.length; i++) {
-      let c = text[i];
-      if (c.match(/[a-z]/i)) {
-        let code = text.charCodeAt(i);
-        // Uppercase letters
-        if (code >= 65 && code <= 90)
-          c = String.fromCharCode(((code - 65 + shift) % 26) + 65);
-        // Lowercase letters
-        else if (code >= 97 && code <= 122)
-          c = String.fromCharCode(((code - 97 + shift) % 26) + 97);
-      }
-      output += c;
-    }
-    this.setState({ encodedOutput: output });
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+    });
   };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
   render() {
-    const letters = this.state.userInput.split("").map((ch, index) => {
-      return (
-        <Char
-          key={index}
-          letter={ch}
-          click={() => this.deleteCharHandler(index)}
-        />
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
+        </div>
       );
-    });
+    }
 
     return (
-      <div className="App">
-        <h1>Ceasar Cipher Generator</h1>
-        <p>
-          With the number of cyber attacks increasing and the fight for privacy
-          has becoming more fierce, <strong>strong</strong> passwords are
-          becoming a lot more important. This tool is a simple Ceasar Cipher
-          which can help make passwords more secure by shifting the letters in
-          your passwords into new letters given the shift number (I.E. Shift=1:
-          A=B, B=C, C=D...)
-        </p>
-
-        <p>
-          Good passwords will have a minimum length of 12 characters, being a
-          mix of letters, numbers, and special characters.
-        </p>
-        <input
-          type="text"
-          value={this.state.shift}
-          onChange={this.shiftChangeHandler}
-          maxLength="2"
-          size="2"
+      <div className={classes.App}>
+        <Cockpit
+          clicked={this.togglePersonHandler}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          title={this.props.appTitle}
         />
-
-        <input
-          type="text"
-          onChange={this.outputHandler}
-          value={this.state.userInput}
-        />
-        <br></br>
-        <button onClick={this.encode}>Encoded Password:</button>
-        <h4>{this.state.encodedOutput}</h4>
-        <Validation input={this.state.userInput} />
-
-        {letters}
-
-        {/* <Char letter={this.state.userInput} /> */}
+        {persons}
       </div>
     );
   }
